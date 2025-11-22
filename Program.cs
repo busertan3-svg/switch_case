34f88226cba996bf7e6e59cfb68ad7f5c75f0f77
BUSENUR ERTAN <BUSENUR.ERTAN@lcwaikiki.com>
2025-11-22 14:34:28 +0300

    Patika.dev C# 101 Switch-Case Odev Kodlari

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            // İlk switch bloğu (ay isimleri için)
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındasınız!");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındasınız!");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan ayındasınız!");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart ayındasınız!");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış veri girişi!");
+                    break;
+            }
+
+            // İkinci switch bloğu (mevsimler için)
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış ayındasınız!");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar ayındasınız!");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz ayındasınız!");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar ayındasınız!"); // Ekran görüntüsünde "Yaz ayındasınız!" yazıyordu, bunu "Sonbahar ayındasınız!" olarak düzelttim
+                    break;
+                default:
+                    Console.WriteLine("Yanlış veri girişi!");
+                    break;
+            }
+        }
+    }
+}
